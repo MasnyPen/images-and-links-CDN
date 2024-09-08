83e28ab568973364df8aef9f98137f7318a27ed8
infnibor <156368035+infnibor@users.noreply.github.com>
2024-09-08 20:27:18 +0200

    Fix Regex

--- public/script.js
+++ public/script.js
@@ -75,46 +75,60 @@ async function uploadFile(event) {
 async function uploadLinks(event) {
   event.preventDefault()
   const field = document.getElementById("link")
-  const link = field.value
+  let link = field.value
   const regex =
-    /^(https?:\/\/)?([a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+)([\/a-zA-Z0-9._-]*)?(\?[a-zA-Z0-9=&_-]*)?(#[a-zA-Z0-9_-]*)?$/
+    /^((https?:\/\/)?(www\.)?([a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+)(:\d+)?(\/[a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=%-]*)?)$/
 
-  const reg = new RegExp(regex)
-  if (!reg.test(link)) {
-    field.value = ""
-    alert("Podany text nie jest linkiem!")
-    return
-  } else {
-    const token = localStorage.getItem("token")
-
-    if (!token) {
-      alert("Musisz się zalogować, aby skrócić link!")
-      return
-    }
-    const formData = new FormData()
-    formData.append("link", link)
-
-    try {
-      const response = await fetch("/links", {
-        method: "POST",
-        headers: {
-          Authorization: token,
-        },
-        body: formData,
-      })
-
-      const data = await response.json()
-
-      if (response.ok) {
-        alert("Link Skrócony pomyślnie! URL: " + data.url)
-      } else {
-        alert("Błąd skracania linku: " + data.error)
+  function normalizeUrl(url) {
+    if (regex.test(url)) {
+      // Jeśli brakuje protokołu, dodaj domyślny "http://"
+      if (!/^https?:\/\//i.test(url)) {
+        return "http://" + url
       }
-    } catch (err) {
-      alert("Wystąpił błąd: " + err.message)
-    } finally {
-      field.value = ""
+      return url // Jeśli jest poprawny, zwróć URL bez zmian
+    } else {
+      return null // Zwraca null dla niepoprawnych URL
     }
+  }
+
+  link = normalizeUrl(link) // Znormalizuj link, nawet jeśli regex go przepuścił
+
+  if (!link) {
+    field.value = ""
+    alert("Podany tekst nie jest prawidłowym linkiem!")
+    return
+  }
+
+  const token = localStorage.getItem("token")
+
+  if (!token) {
+    alert("Musisz się zalogować, aby skrócić link!")
+    return
+  }
+
+  const formData = new FormData()
+  formData.append("link", link)
+
+  try {
+    const response = await fetch("/links", {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      alert("Link skrócony pomyślnie! URL: " + data.url)
+    } else {
+      alert("Błąd skracania linku: " + data.error)
+    }
+  } catch (err) {
+    alert("Wystąpił błąd: " + err.message)
+  } finally {
+    field.value = ""
   }
 }
 
